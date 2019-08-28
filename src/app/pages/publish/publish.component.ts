730ae50d4b40ea6979dbabe9cf917dda05f9d6c2
nyazkhan <khannyaz1705@gmail.com>
2019-08-28 18:24:25 +0530

    start working on publish

---
+++ src/app/pages/publish/publish.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+declare const $: any;
+@Component({
+  selector: 'app-publish',
+  templateUrl: './publish.component.html',
+  styleUrls: ['./publish.component.css']
+})
+export class PublishComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+}
